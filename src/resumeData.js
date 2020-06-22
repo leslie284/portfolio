1e29c3ba0edc4b1f74ee22140a17e3174e198342
leslie284 <lesliedallapiccola02@gmail.com>
2020-06-22 11:46:16 +1000

    portfolio-first-version

---
+++ src/resumeData.js
@@ -0,0 +1,113 @@
+let resumeData = {
+  "imagebaseurl": "#",
+  "name": "Leslie Katerina Dallapiccola",
+  "role": "Sr.MERN & Front-End Dev ",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "live:.cid.24022fd53980731",
+  "roleDescription": " I am a full time freelancer and I love my jobs. I always keep timeline and high quality code. I will be available for discussion whenever you want and you can ask whatever belong to my skill set.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/leslie-67ba08121/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/leslie284",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "https://join.skype.com/gFzGpeTC4CTY",
+      "className": "fa fa-skype"
+    }
+  ],
+  "aboutme": "I am React developer from Argentina with 6 years of experience in web development. From the simple web site to Complicated multiple web app I have developed with my expertise.I have rich experience in landing page, responsive web page, SPA, fixing bug and issue, refactoring messy code, analyzing codebase and adding features to existing web applications. As a full time developer, I can be available for discussion about projects when you require.I will do my best to deliver you best quality code. My Proficient skills : HTML5, CSS3, BOOTSTRAP Javascript, jQuery, Ajax React, React Bootstrap, Material UI, Redux Node.js, Express API Integration Stripe Amazon Web Service",
+  "address": "San Martin de Los Andes, Argentina",
+  "website": "https://leslie284",
+  "education": [
+    {
+      "UniversityName": "Universidad Nacional de La Plata",
+      "specialization": "Bachelor of Computer Science",
+      "MonthOfPassing": "Aug",
+      "YearOfPassing": "2017",
+      "Achievements": "I have learned Basic web terminologies and some skills when it comes to Web."
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "AXU",
+      "specialization": "Web Developer",
+      "YearOfEntrance": "October 2017 -",
+      "YearOfLeaving": "May 2018",
+      "Achievements": "I have assisted to the Medium-scale project with Front-End. Especially, I have built Landing page and integrated some APIs. It was React.Js Project. Except for this, I had gotten involved in a few projects at the time."
+    }
+  ],
+  "skillsDescription": "My main skills are as following.",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "javascript"
+    },
+    {
+      "skillname": "BootStrap"
+    },
+    {
+      "skillname": "React"
+    },
+    {
+      "skillname": "Nodejs"
+    },
+    {
+      "skillname": "MongoDB"
+    },
+    {
+      "skillname": "Express"
+    },
+    {
+      "skillname": "Laravel"
+    },
+    {
+      "skillname": "CodeIgniter"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
