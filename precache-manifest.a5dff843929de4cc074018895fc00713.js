3eec5938e395bd5fa2fbde88741ea44aa82790ed
leslie284 <lesliedallapiccola02@gmail.com>
2020-06-22 13:28:52 +1000

    Updates

---
+++ precache-manifest.a5dff843929de4cc074018895fc00713.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f96d9dd1144b45a96b97",
+    "url": "/portfolio/static/css/main.230e01fd.chunk.css"
+  },
+  {
+    "revision": "f96d9dd1144b45a96b97",
+    "url": "/portfolio/static/js/main.f96d9dd1.chunk.js"
+  },
+  {
+    "revision": "6221052083d9d7c06dcf",
+    "url": "/portfolio/static/js/1.62210520.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "bd3680d3ce96c83ebdd8a94ce99a6aaa",
+    "url": "/portfolio/index.html"
+  }
+];
